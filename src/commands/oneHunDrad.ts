58d0cc092177772e01fd144829accbe088d8455b
rukchanok krumsamer <pepoil07@hotmail.com>
2023-08-29 22:40:08 +0700

    Bug can get message

---
+++ src/commands/oneHunDrad.ts
@@ -0,0 +1,57 @@
+import { EmbedBuilder, SlashCommandBuilder } from "discord.js";
+import { Command } from "../interface/Command";
+import { getCamperData } from "../modules/getCamperData";
+import { updateCamperdata } from "../modules/updateCamperData";
+
+export const onehHunDred: Command = {
+    data: new SlashCommandBuilder()
+        .setName("100")
+        .setDescription("Check in for the 100 Days of Code challenge.")
+        .addStringOption((option) =>
+            option
+                .setName("message")
+                .setDescription(
+                    "The message to go in your 100 Days of Code update."
+                )
+                .setRequired(true)
+                .addChoices({
+                    name: "100",
+                    value: "message_100"
+                })
+        ),
+    run: async (interaction) => {
+        await interaction.deferReply();
+        const { user } = interaction;
+        const text = interaction.options.get("message", true).message;
+        const targetCamper = await getCamperData(user.id);
+        const updateCamper = await updateCamperdata(targetCamper);
+
+        const onehHunDredEmbed = new EmbedBuilder()
+            .setColor(0x0099ff)
+            .setTitle("Test bot 100 days naja !!")
+            .setDescription(text)
+            .setAuthor({
+                name: user.tag,
+                iconURL: user.defaultAvatarURL
+            })
+            .addFields([
+                {
+                    name: "Round",
+                    value: updateCamper.round.toString(),
+                    inline: true
+                },
+                {
+                    name: "Day",
+                    value: updateCamper.day.toString(),
+                    inline: true
+                }
+            ])
+            .setFooter({
+                text:
+                    "Day completed: " +
+                    new Date(updateCamper.timestamp).toLocaleDateString()
+            });
+
+        await interaction.editReply({ embeds: [onehHunDredEmbed] });
+    }
+};
